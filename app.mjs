37cae9668d7041e069f275dee2cf81bec5f6f5e5
juansanchez <alzatejuan1980@gmail.com>
2024-02-03 20:17:08 -0500

    ni recuerdo q cambie

--- app.mjs
+++ app.mjs
@@ -7,6 +7,12 @@ config();
 
 const app = express();
 
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error interno del servidor');
+  });
+  
 app.set("port", process.env.port || 3001);
 app.use(cors());
 app.use(rutaMusica);
--- contoles/musicaControl.mjs
+++ contoles/musicaControl.mjs
@@ -1,5 +1,23 @@
-import fs from "fs"
+import fs from "fs";
+import path from "path";
 
-export const archivoMusica = async(req,res)=>{
-    res.json('oe')
-}
+const basePath = 'C:\\Users\\juan1\\Documents';
+const folderName = 'canciones';
+
+const fullPath = path.resolve(basePath, folderName);
+
+export const archivoMusica = (req, res) => {
+    try {
+        const archivos = fs.readdirSync(fullPath);
+        let canciones = archivos.map((archivo, index) => ({
+            id: index + 1,  // Asigna un id único basado en el índice del archivo
+            nombre: archivo,
+            url: `http://localhost:5000/canciones/${archivo}` // Utiliza una URL válida
+        }));
+      
+        res.json(canciones);
+    } catch (error) {
+        console.error("Error al leer el directorio:", error);
+        res.status(500).json('Error en el servidor');
+    }
+};
